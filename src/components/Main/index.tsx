6786d39d57ed9da789ec882f0db999200387756f
Ian Peres Coelho <ian.coel@hotmail.com>
2021-12-30 19:36:59 -0300

    useEffect para puxar dados da api

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, UserAge, UserCard, UserName } from './styles';
+import api from '../../services';
 
 const Main: React.FC = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    api.get('/users').then((response) => {
+      console.log(response.status);
+      setUsers(response.data);
+    });
+  }, []);
+
   return (
     <Container>
+      <button onClick={() => console.log(users)}>Clique-me</button>
       <UserCard>
         <UserName>Ian</UserName>
         <UserAge>25 anos</UserAge>
-      </UserCard>
-      <UserCard>
-        <UserName>Ian</UserName>
-        <UserAge>25 anos</UserAge>
-      </UserCard>
-      <UserCard>
-        <UserName>Ian</UserName>
-        <UserAge>25 anos</UserAge>
-      </UserCard>
-      <UserCard>
-        <UserName>Ian</UserName>
-        <UserAge>25 anos</UserAge>
-      </UserCard>
-      <UserCard>
-        <UserName>Ian</UserName>
-        <UserAge>25 anos</UserAge>
+
+        {users?.map((user) => (
+          <UserCard>
+            <UserName>{user.name}</UserName>
+            <UserAge></UserAge>
+          </UserCard>
+        ))}
       </UserCard>
     </Container>
   );
