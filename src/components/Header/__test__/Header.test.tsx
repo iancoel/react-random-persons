e40e376386b490c3bcac5ba5e346f3ad8d5f39fb
Ian Peres Coelho <ian.coel@hotmail.com>
2022-01-04 17:26:42 -0300

    Adicionado testes unitários para o Header

--- src/components/Header/__test__/Header.test.tsx
+++ src/components/Header/__test__/Header.test.tsx
@@ -1,27 +1,66 @@
 /* eslint-disable testing-library/no-debugging-utils */
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Header from '../index';
 import { Provider } from 'react-redux';
 import store from '../../../store/store';
 
-describe('Header', () => {
-  test('Renderiza os componentes', () => {
+describe('Renderiza os componentes do Header', () => {
+  test('Renderiza o título', () => {
     render(
       <Provider store={store}>
         <Header />
       </Provider>,
     );
     const headerTitle = screen.getByText('random-users');
-    const ageInput = screen.getByPlaceholderText('Idade entre 0 e 98');
-    const ageButton = screen.getByText('Buscar por idade');
-    const nameInput = screen.getByPlaceholderText('Nome');
-    const nameButton = screen.getByText('Buscar por nome');
-    const allUsersButton = screen.getByText('Mostrar todos os usuários');
 
     expect(headerTitle).toBeInTheDocument();
+  });
+
+  test('Consegue-se digitar no input de idade', () => {
+    render(
+      <Provider store={store}>
+        <Header />
+      </Provider>,
+    );
+
+    const ageInput: HTMLInputElement =
+      screen.getByPlaceholderText('Idade entre 0 e 98');
+
+    fireEvent.change(ageInput, { target: { value: 25 } });
+
     expect(ageInput).toBeInTheDocument();
-    expect(ageButton).toBeInTheDocument();
+    expect(ageInput.value).toBe('25');
+  });
+
+  test('Consegue-se digitar no input de nome', () => {
+    render(
+      <Provider store={store}>
+        <Header />
+      </Provider>,
+    );
+
+    const nameInput: HTMLInputElement = screen.getByPlaceholderText('Nome');
+
+    fireEvent.change(nameInput, { target: { value: 'Marina' } });
+
     expect(nameInput).toBeInTheDocument();
+    expect(nameInput.value).toBe('Marina');
+  });
+
+  test('Botões renderizados', () => {
+    render(
+      <Provider store={store}>
+        <Header />
+      </Provider>,
+    );
+
+    const nameButton: HTMLButtonElement = screen.getByRole('button', {
+      name: 'Buscar por nome',
+    });
+    const allUsersButton: HTMLButtonElement = screen.getByText(
+      'Mostrar todos os usuários',
+    );
+
     expect(nameButton).toBeInTheDocument();
     expect(allUsersButton).toBeInTheDocument();
   });
